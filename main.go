438764e4b0885ad21ad6c6f538866e7213b48071
Fareez Ahamed <fareez.ahamed@gmail.com>
2020-07-19 13:17:08 +0530

    Started using os's temp folder; fixes #3

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"flag"
 	"fmt"
 	"html/template"
+	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 )
 
 var layoutPath = flag.String("template", "layout/layout.html", "HTML template used to generate pdf document")
-var tmpPath = flag.String("temp", "tmp", "Temporary folder to keep generated HTML docs")
 var pdfPath = flag.String("out", "pdf", "Output folder")
 var csvFilePath = flag.String("in", "input.csv", "Input CSV file path")
 
@@ -21,17 +22,26 @@ func main() {
 
 	headers, data := getCsvData(*csvFilePath)
 
-	fmt.Printf("Total number of records read : %d", len(data))
+	fmt.Printf("Total number of records read : %d\n", len(data))
 
 	dataMap := make(map[string]string)
 
+	tempDir, err := ioutil.TempDir("", "go_receipt*")
+	if err != nil {
+		log.Fatalf(fmt.Sprintf("Unable to create a temporary folder: %v", err))
+	}
 	for _, record := range data {
 		for i, field := range record {
 			dataMap[headers[i]] = field
 		}
 		fileID := dataMap[fileIDFieldName]
-		filename := generateHTML(dataMap, fileID)
+		filename := generateHTML(dataMap, tempDir, fileID)
 		generatePdf(filename, fileID)
+	}
+
+	err = os.RemoveAll(tempDir)
+	if err != nil {
+		log.Fatalf(fmt.Sprintf("Unable to delete temporary folder: %v", err))
 	}
 }
 
@@ -57,15 +67,18 @@ func getCsvData(filename string) ([]string, [][]string) {
 	return headers, data[1:]
 }
 
-func generateHTML(data map[string]string, fileID string) string {
+func generateHTML(data map[string]string, tempDir, fileID string) string {
 	tmpl, err := template.ParseFiles(*layoutPath)
 	if err != nil {
 		return ""
 	}
 
-	filename := *tmpPath + "/" + fileID + ".html"
+	filename := tempDir + "/" + fileID + ".html"
 
 	outputFile, err := os.Create(filename)
+
+	defer outputFile.Close()
+
 	if err != nil {
 		return ""
 	}
